5cd44dde3a2de7e9bbc820bb98596b10d747fecb
Prafull Saxena <ps.prafullsaxena0@gmail.com>
2022-03-20 14:42:58 +0530

    Created v1.0 deploye

---
+++ dist/portfolio/assets/Objects/ProjectObject.ts
@@ -0,0 +1,12 @@
+
+export class Project {
+
+    constructor(
+        public svgLocation : string,
+        public projectName : string,
+        public projectInfo : string,
+        public projectURL : string,
+        public demoUrl : string,
+        public isDemoAvailable :boolean) { }
+
+} 
---
+++ dist/portfolio/assets/Objects/SkillsIcons.ts
@@ -0,0 +1,12 @@
+
+export class skillsIcons{
+
+
+    constructor(
+        public path : string,
+        public name : string
+    ){
+        this.path = path;
+        this.name = name;
+    }
+}
